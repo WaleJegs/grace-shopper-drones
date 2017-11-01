24581b763741b89182317c3c7c6523a632d4cf29
Wale Jegs <wale.a.jegede@gmail.com>
2017-11-01 15:29:00 -0400

    message

--- server/api/users.js
+++ server/api/users.js
@@ -1,14 +1,50 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  User.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-    attributes: ['id', 'email']
-  })
-    .then(users => res.json(users))
-    .catch(next)
+    User.findAll({
+            // explicitly select only the id and email fields - even though
+            // users' passwords are encrypted, it won't help if we just
+            // send everything to anyone who asks!
+            attributes: ['id', 'email']
+        })
+        .then(users => res.json(users))
+        .catch(next)
 })
+
+router.get('/:userId', (req, res, next) => {
+    User.findById(req.params.userId, {
+            attributes: ['id', 'email']
+        })
+        .then((user) => {
+            res.json(user)
+        })
+        .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+    User.create(req.body)
+        .then(user => {
+            res.json(user.id)
+        })
+        .catch(next);
+});
+
+router.put('/:userId', (req, res, next) => {
+    User.findById(req.params.userId)
+        .then(user => {
+            user.update(req.body)
+            res.send(200)
+        })
+        .catch(next)
+})
+
+router.delete('/:userId', (req, res, next) => {
+    User.findById(req.params.userId)
+        .then(user => {
+            user.destroy({});
+            res.send(201)
+        })
+        .catch(next);
+});
