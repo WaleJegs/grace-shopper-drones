9519c2c4bbf7c017d63864896e2b3b41883f700b
Yuchen Chen <yuchchen1314@gmail.com>
2017-11-04 18:52:25 -0400

    going to fix first item

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -14,7 +14,7 @@ class SingleProduct extends Component{
 
     handleClick(e){
         if (!window.localStorage.getItem("cart")){
-            window.localStorage.setItem("cart", this.props.singleProduct.id+"-"+this.props.singleProduct.price+"-"+this.props.singleProduct.name)
+            window.localStorage.setItem("cart", "next"+"-"+this.props.singleProduct.id+"-"+this.props.singleProduct.price+"-"+this.props.singleProduct.name)
             this.props.addToCartAction(window.localStorage.getItem('cart').split('-')[0])
             console.log(window.localStorage.getItem("cart"))
         } else {
